ea6e30a072d61cc1a01d04b77469010d229d57af
Jarl Friis <jarl@softace.dk>
2012-09-11 08:32:23 +0200

    Initial project from 'rails new vanilla_app --skip-active-record --skip-sprockets --skip-javascript --skip-test-unit --edge --skip-bundle'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VanillaApp::Application.config.secret_token = '198ec6534ce469a6297c5e4091577687520f36051f5548367daf88f369d458c8d93b5105e114dd80ea5e0c86565e38742b77151eb01fd4480a57e610fb556a9e'
